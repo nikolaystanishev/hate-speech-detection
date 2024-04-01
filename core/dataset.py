ea9573e0b09ea17a1abc8d84e820ad1302bc2ade
nikolaystanishev <stanishev.nikolay@gmail.com>
2024-04-01 15:04:38 +0200

    Add dataset for the facebook hateful memes dataset

---
+++ core/dataset.py
@@ -0,0 +1,30 @@
+import os
+import json
+from PIL import Image
+from torch.utils.data import Dataset
+
+
+class HatefulMemesDataset(Dataset):
+
+    def __init__(self, data_file_path, tokenizer=None, transform=None):
+        self.data = [json.loads(jline) for jline in open(data_file_path, 'r').readlines()]
+        self.data_dir = os.path.dirname(data_file_path)
+        self.tokenizer = tokenizer
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        el = self.data[idx]
+
+        img = Image.open(os.path.join(self.data_dir, el['img']))
+        text = el['text']
+        label = el['label']
+
+        if self.transform:
+            img = self.transform(img)
+        if self.tokenizer:
+            text = self.tokenizer(text)
+
+        return img, text, label
