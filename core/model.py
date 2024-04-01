a3ca8388d9b5213ca614689b4447d5e0f2d418a5
nikolaystanishev <stanishev.nikolay@gmail.com>
2024-04-01 16:19:22 +0200

    Add initial model

---
+++ core/model.py
@@ -0,0 +1,42 @@
+import torch
+from torch.nn import Module, Sequential, Linear
+from transformers import BertModel
+import torchvision.models as models
+
+class HateMemeModel(Module):
+
+    def __init__(self, text_model, image_model):
+        super(HateMemeModel, self).__init__()
+        self.text_model = text_model
+        self.image_model = image_model
+        
+        self.fc1 = Linear(768 + 2048, 1024)
+        self.fc2 = Linear(1024, 256)
+        self.fc3 = Linear(256, 2)
+
+        self.relu = torch.nn.ReLU()
+
+    def forward(self, text, image):
+        text_features = self.text_model(**text).pooler_output
+        image_features = self.image_model(image)
+        image_features = image_features.view(image_features.size(0), -1)
+        
+        combined = torch.cat([text_features, image_features], dim=1)
+        out = self.relu(self.fc1(combined))
+        out = self.relu(self.fc2(out))
+        out = self.fc3(out)
+        
+        return out
+
+
+class PretrainedModel:
+
+    @staticmethod
+    def load_bert_text_model():
+        return BertModel.from_pretrained('bert-base-uncased')
+    
+    @staticmethod
+    def load_resnet_image_model():
+        model = models.resnet50(pretrained=True)
+        modules = list(model.children())[:-1]
+        return Sequential(*modules)
