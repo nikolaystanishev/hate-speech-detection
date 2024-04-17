c85f9c75be2fabcd1925bc1ff0f80690feee03f6
nikolaystanishev <stanishev.nikolay@gmail.com>
2024-04-17 11:23:25 +0200

    Fight with overfitting

--- core/model.py
+++ core/model.py
@@ -5,24 +5,28 @@ import torchvision.models as models
 
 class HateMemeModel(Module):
 
-    def __init__(self, text_model, image_model):
+    def __init__(self, text_model, image_model, dropout=0.1):
         super(HateMemeModel, self).__init__()
         self.text_model = text_model
+        self.batch_norm1 = torch.nn.BatchNorm1d(768)
         self.image_model = image_model
+        self.batch_norm2 = torch.nn.BatchNorm1d(512)
         
-        self.fc1 = Linear(768 + 2048, 1024)
-        self.fc2 = Linear(1024, 256)
+        # 2816
+        self.fc1 = Linear(768 + 512, 512)
+        self.fc2 = Linear(512, 256)
         self.fc3 = Linear(256, 2)
 
         self.relu = torch.nn.ReLU()
+        self.dropout = torch.nn.Dropout(dropout)
 
     def forward(self, text, image):
-        text_features = self.relu(self.text_model(**text).pooler_output)
-        image_features = self.relu(self.image_model(image))
-        image_features = image_features.view(image_features.size(0), -1)
+        text_features = self.relu(self.batch_norm1(self.text_model(**text).pooler_output))
+        image_features = self.image_model(image)
+        image_features = self.relu(self.batch_norm2(image_features.view(image_features.size(0), -1)))
         
         combined = torch.cat([text_features, image_features], dim=1)
-        out = self.relu(self.fc1(combined))
+        out = self.dropout(self.relu(self.fc1(combined)))
         out = self.relu(self.fc2(out))
         out = self.fc3(out)
         
@@ -41,6 +45,6 @@ class PretrainedModel:
     
     @staticmethod
     def load_resnet_image_model():
-        model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
+        model = models.resnet34(weights=models.ResNet34_Weights.DEFAULT)
         modules = list(model.children())[:-1]
         return Sequential(*modules)
