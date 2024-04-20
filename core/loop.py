cce6956d6e252728c85551345d17dc23a0b4a59c
andreac01 <andrea01.cerutti@gmail.com>
2024-04-20 11:04:11 +0200

    created notebook and implemented hateclipper

--- core/loop.py
+++ core/loop.py
@@ -1,6 +1,6 @@
 import torch
 import numpy as np
-from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score, roc_auc_score
 from tqdm import tqdm
 
 
@@ -78,16 +78,18 @@ def train_freeze(model, train_loader, val_loader, optimizer, criterion, epochs, 
             optimizer.zero_grad()
 
             image, text, label = batch
-
             output = model(text, image)
-            loss = criterion(output, label)
+            # label_tensor = torch.nn.functional.one_hot(label).to(dtype=torch.float32) # if using BCEWithLogitsLoss with 2 output
+            # label = label.reshape(-1, 1).to(dtype=torch.float32) # if using BCEWithLogitsLoss with 1 output
+
+            loss = criterion(output['text_image'], label) + criterion(output['image'], label) + criterion(output['text'], label)
+            #torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1e-1)
 
             train_losses.append(loss.item())
             loss.backward()
             optimizer.step()
 
         torch.save(model.state_dict(), f'{model_dir_path}/model_freeze_{epoch + 1}.pth')
-
         print(f'Epoch {epoch + 1} average loss: {np.mean(train_losses):.3f}')
         evaluate_freeze(model, train_loader, criterion, device, 'Train')
         evaluate_freeze(model, val_loader, criterion, device)
@@ -99,10 +101,18 @@ def evaluate_freeze(model, data_loader, criterion, device, kind='Evaluation'):
             image, text, label = batch
 
             out = model(text, image)
-            loss = criterion(out, label)
+            # label_tensor = torch.nn.functional.one_hot(label).to(dtype=torch.float32) # if using BCEWithLogitsLoss with 2 output
+            # label = label.reshape(-1, 1).to(dtype=torch.float32) # if using BCEWithLogitsLoss with 1 output
+
+            loss = criterion(out['text_image'], label) + criterion(out['image'], label) + criterion(out['text'], label)
             losses.append(loss.item())
 
-            pred = np.argmax(torch.sigmoid(out).cpu().detach().numpy(), axis=1)
+            # voting mechanism
+            pred1 = np.argmax((torch.sigmoid(out['text_image'])).cpu().detach().numpy(), axis=1)
+            pred2 = np.argmax((torch.sigmoid(out['image'])).cpu().detach().numpy(), axis=1)
+            pred3 = np.argmax((torch.sigmoid(out['text'])).cpu().detach().numpy(), axis=1)
+            pred = np.mean([pred1, pred2, pred3], axis=0)
+            pred = np.round(pred)
 
             preds.append(pred.tolist())
             labels.append(label.cpu().numpy())
@@ -116,8 +126,11 @@ def evaluate_freeze(model, data_loader, criterion, device, kind='Evaluation'):
         metrics["accuracy"] = accuracy_score(labels, preds)
         metrics["precision"] = precision_score(labels, preds)
         metrics["recall"] = recall_score(labels, preds)
+        metrics["ROC-AUC"] = roc_auc_score(labels, preds)
 
         print(
             f'{kind} loss: {metrics["loss"]:.3f}, macro f1: {metrics["macro_f1"]:.3f}, ' + \
             f'micro f1: {metrics["micro_f1"]:.3f}, accuracy: {metrics["accuracy"]:.3f}, ' + \
-            f'precision: {metrics["precision"]:.3f}, recall: {metrics["recall"]:.3f}')
+            f'precision: {metrics["precision"]:.3f}, recall: {metrics["recall"]:.3f}, ' + \
+            f'ROC-AUC: {metrics["ROC-AUC"]:.3f}'
+        )
--- training_clip_freeze.py
+++ training_clip_freeze.py
@@ -8,13 +8,17 @@ from torch import nn, optim
 from core.model import ClipHateMemeModelFreeze, PretrainedModel
 from core.dataset import ClipHatefulMemeDatasetFreeze
 from core.loop import train_freeze, evaluate_freeze
+from torchsummary import summary
+from torch.nn import functional as F
+import numpy as np
 
 cur_dir = os.path.dirname(__file__)
 DATA_DIR = os.path.join(cur_dir, 'data/')
 
 
+
 def main():
-	lr = 2e-4
+	lr = 5e-4
 	epochs = 20
 	batch_size = 64
 	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -27,11 +31,13 @@ def main():
 	val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
 
 	model = ClipHateMemeModelFreeze()
-	optimizer = optim.AdamW(model.parameters(), lr=lr, eps=1e-4, weight_decay=0.01)
-	criterion = nn.CrossEntropyLoss(weight=torch.tensor([1.0/5481, 1/3019]))
+	optimizer = optim.AdamW(model.parameters(), lr=lr, weight_decay=1e-1)
+	criterion = nn.CrossEntropyLoss(weight=torch.tensor([1000/5481, 1000/3019]), label_smoothing=0.2)
+	#criterion = nn.BCEWithLogitsLoss(reduction='mean', weight=torch.tensor([1.0/5481, 1/3019]))#, pos_weight=torch.ones([1,1]))#, label_smoothing=0.01)
 
 	model.to(device)
 	criterion.to(device)
+	print(summary(model, input_size=[(1, 768), (1, 768)]))
 
 	train_freeze(
 	model, train_loader, val_loader, optimizer, criterion, epochs, device,
